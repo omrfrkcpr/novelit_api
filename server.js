3f485de183ddd98c14d35be84f0e0a639365ebb2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-19 18:06:45 +0200

    "Added cors dependency and middleware to server.js, updated package.json and package-lock.json accordingly"

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 "use strict";
 const express = require("express");
+const cors = require("cors");
+
 require("express-async-errors");
+
+const corsOptions = {
+  origin: `http://localhost:${process.env.PORT}`,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 const app = express();
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.all("/", (req, res) => {
   res.send("Library Management APP");
